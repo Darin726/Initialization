2a41ec123ab0c4c131aed25b1259722c71299004
Darin <dyy726@qq.com>
2016-02-27 20:46:01 +0800

    init Master

---
+++ src/main/java/cn/qianlicao/initialization/Status.java
@@ -0,0 +1,16 @@
+package cn.qianlicao.initialization;
+
+/**
+ * Created by dongyayun on 16/2/22.
+ */
+public class Status {
+    public static final int STATUS_INIT = 0;
+
+    public static final int STATUS_WAITING = 1;
+
+    public static final int STATUS_DOING = 2;
+
+    public static final int STATUS_DONE = 3;
+
+
+}
